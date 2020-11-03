97aa3343c3c1c73bdcf5b7840fc9df5be2ea6bee
HIBO <okhaibo@gmail.com>
2020-11-03 21:18:11 +0800

    从头到尾打印链表

---
+++ cn/[剑指 Offer 06]从尾到头打印链表.java
@@ -0,0 +1,63 @@
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1] 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 链表长度 <= 10000 
+// Related Topics 链表 
+// 👍 69 👎 0
+
+
+package leetcode.editor.cn;
+
+class CongWeiDaoTouDaYinLianBiaoLcof {
+    public static void main(String[] args) {
+        Solution solution = new CongWeiDaoTouDaYinLianBiaoLcof().new Solution();
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public int[] reversePrint(ListNode head) {
+            // 先获取链表的长度
+            ListNode cur = head;
+            int size=0;
+            while(cur!=null){
+                size++;
+                cur = cur.next;
+            }
+            int[] res = new int[size];
+            while(head!=null){
+                res[--size] = head.val;
+                head = head.next;
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
