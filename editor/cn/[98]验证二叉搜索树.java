1f49a2ce79cca83f701b2b3f0a6970050f52b8f0
Highball <okhaibo@gmail.com>
2022-10-31 23:41:22 +0800

    验证二叉搜索树

--- editor/cn/[98]验证二叉搜索树.java
+++ editor/cn/[98]验证二叉搜索树.java
@@ -41,44 +41,48 @@ package leetcode.editor.cn;
 
 import java.util.List;
 
-class ValidateBinarySearchTree {
-    public static void main(String[] args) {
-        Solution solution = new ValidateBinarySearchTree().new Solution();
+class ValidateBinarySearchTree{
+  public static void main(String[] args) {
+       Solution solution = new ValidateBinarySearchTree().new Solution();
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // 二叉搜索树的中序遍历是从小到大排序的
+    long minValue = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        return inorder(root);
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
 
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     * int val;
-     * TreeNode left;
-     * TreeNode right;
-     * TreeNode() {}
-     * TreeNode(int val) { this.val = val; }
-     * TreeNode(int val, TreeNode left, TreeNode right) {
-     * this.val = val;
-     * this.left = left;
-     * this.right = right;
-     * }
-     * }
-     */
-    class Solution {
-        long pre = Long.MIN_VALUE;
-
-        public boolean isValidBST(TreeNode root) {
-            if (root == null) {
-                return true;
-            } // 访问左子树
-            if (!isValidBST(root.left)) {
-                return false;
-            } // 访问当前节点：如果当前节点小于等于中序遍历的前一个节点，说明不满足BST，返回 false；否则继续遍历。
-            if (root.val <= pre) {
-                return false;
-            }
-            pre = root.val; // 访问右子树
-            return isValidBST(root.right);
+    private boolean inorder(TreeNode root) {
+        if (root == null){
+            return true;
         }
+        // 左，中，右
+        if (!inorder(root.left)) {
+            return false;
+        }
+        if (root.val <= minValue){
+            return false;
+        }
+        minValue = root.val;
+        return inorder(root.right);
     }
+}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
